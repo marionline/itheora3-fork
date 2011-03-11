665b48a87b873e12ee55081a8954dc0e4d33a323
Mario Santagiuliana <mario@marionline.it>
2011-03-11 23:03:18 +0100

    Add new branch to video-js with code to extend object tag

---
+++ config/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  Add poster imate to VideoJS?
+ */
+$poster_image = true;
+/**
+ * Add MP4 source to VideoJS?
+ */
+$MP4_source = true;
+/**
+ * Add WEBM source to VideoJS?
+ */
+$WEBM_source = true;
+/**
+ * Use VideoJS flash fallback?
+ */
+$flash_fallback = false;
