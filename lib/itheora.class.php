38c8b9bb9acc6af27cd96ef9c3b95128695c9fe7
Mario Santagiuliana <mario@marionline.it>
2011-03-18 16:03:55 +0100

    Add external video support

--- lib/itheora.class.php
+++ lib/itheora.class.php
@@ -11,10 +11,17 @@ require_once('lib/functions.php');
 class itheora {
 
     protected $_videoName = 'example'; // Default value of videoName
+    protected $_videoErrorName = 'error';
     protected $_videoStoreDir;
     protected $_cacheDir;
     protected $_baseUrl;
     protected $_files = array();
+    protected $_externalVideo = false; // Is set external video?
+    protected $_externalUrl = ''; // External video URL if video is not locally
+    protected $_supported_video = array('ogg', 'ogv', 'webm', 'mp4');
+    protected $_supported_image = array('png', 'jpg', 'gif');
+    protected $_mimetype_video = array();
+    protected $_mimetype_image = array();
 
     /**
      * __construct 
@@ -23,21 +30,120 @@ class itheora {
      * @return void
      */
     function __construct() {
+	// Create supported mimetype image and video
+	foreach($this->_supported_image as $extension){
+	    if($extension == 'jpg')
+		$extension = 'jpeg';
+	    $this->_mimetype_image[] = 'image/' . $extension;
+	}
+	foreach($this->_supported_video as $extension){
+	    $this->_mimetype_video[] = 'video/' . $extension;
+	}
 	// Local video store directory
 	$this->_videoStoreDir = dirname(__FILE__) . '/../video';
+
 	// Local cache directory
 	$this->_cacheDir = dirname(__FILE__) . '/../cache';
+
 	// The name of the server host
-	//$this->_baseUrl = strtolower(substr($_SERVER['SERVER_PROTOCOL'], 0, 5)) == 'https://' ? 'https://' : 'http://' 
-	    //. $_SERVER['HTTP_HOST']
-	    //. pathinfo($_SERVER['PHP_SELF'], PATHINFO_DIRNAME);
 	$this->_baseUrl = getBaseUrl();
 
 	// Check if cache directory exist and is writable, if not writable change chmod
 	if(is_dir($this->_cacheDir) && !is_writable($this->_cacheDir))
 	    chmod($this->_cacheDir, 0755); // 755 should be ok
 
+	// Get file, default is example
 	$this->getFiles();
+    }
+
+    /**
+     * completeUrl 
+     * 
+     * @param string $file 
+     * @access protected
+     * @return string
+     */
+    protected function completeUrl($file) {
+	if($this->_externalVideo){
+	    return $this->_externalUrl . $file;
+	} else {
+	    return $this->_baseUrl . '/video/' . $this->_videoName . '/' . $file;
+	}
+    }
+
+    /**
+     * check_founded_files 
+     * Check if $this->_files store some files or not
+     * 
+     * @access protected
+     * @return void | true if a file is founded
+     */
+    protected function check_founded_files() {
+        if (count($this->_files) == 0)
+	    return false;
+	else
+	    return true;
+    }
+
+    /**
+     * is_supported_image 
+     * 
+     * @param mixed $file_headers Pass headers with get_headers($file, 1) 
+     * @access protected
+     * @return bool
+     */
+    protected function is_supported_image($file_headers) {
+	// Use in_array function to check if mime-type is supported_video
+	if(isset($file_headers['Content-Type'])){
+	    return in_array($file_headers['Content-Type'], $this->_mimetype_image);
+	} else {
+	    throw new Exception('$file_headers[\'Content-Type\'] not set, please pass $file_headers with get_headers($file, 1).');
+	}
+    }
+    /**
+     * is_supported_video 
+     * 
+     * @param mixed $file_headers Pass headers with get_headers($file, 1)
+     * @access protected
+     * @return bool
+     */
+    protected function is_supported_video($file_headers) {
+	// Use in_array function to check if mime-type is supported_video
+	if(isset($file_headers['Content-Type'])){
+	    return in_array($file_headers['Content-Type'], $this->_mimetype_video);
+	} else {
+	    throw new Exception('$file_headers[\'Content-Type\'] not set, please pass $file_headers with get_headers($file, 1).');
+	}
+    }
+
+    /**
+     * getExternalFiles 
+     * 
+     * @access protected
+     * @return bool
+     */
+    protected function getExternalFiles() {
+	$extensions = array_merge($this->_supported_video, $this->_supported_image);
+	foreach($extensions as $extension) {
+	    // Basename file
+	    $file = $this->_videoName . '.' . $extension;
+	    // Get headers of $file
+	    $headers = get_headers($this->completeUrl($file), 1);
+	    // Check if HTTP respons is not a 404 error
+	    if(substr($headers[0], 9, 3) != '404') {
+		// Check if file is supported
+		if($this->is_supported_image($headers) || $this->is_supported_video($headers)) {
+		    $this->_files[$extension] = $file;
+		}
+	    }
+	}
+
+	$files=$this->_files;
+	if(!$this->check_founded_files()) {
+	    return $this->setVideoName($this->_videoErrorName);
+	} else {
+	    return true;
+	}
     }
 
     /**
@@ -51,39 +157,79 @@ class itheora {
     }
 
     /**
-     * completeUrl 
-     * 
-     * @param string $file 
-     * @access protected
-     * @return string
-     */
-    protected function completeUrl($file) {
-	return $this->_baseUrl . '/video/' . $this->_videoName . '/' . $file;
-    }
-
-    /**
-     * getFiles 
+     * getLocalFiles 
      * 
      * @access protected
-     * @return void | false
+     * @return bool False if no file are found
      */
-    protected function getFiles() {
-	// Get the files, video and picture to use
+    protected function getLocalFiles() {
 	if(is_dir($this->video())){
-	    if ($handle = opendir($this->video()) ) {
+	    if ( $handle = opendir($this->video()) ) {
 		while (false !== ($file = readdir($handle))) {
 		    $this->_files[pathinfo($file, PATHINFO_EXTENSION)] = $file;
 		}
 		closedir($handle);
 	    }
 	} else {
-	    if($this->_videoName != 'error'){
-		$this->setVideoName('error');
-		$this->getFiles();
-	    } else {
-		return false;
+	    if($this->_videoName != $this->_videoErrorName) {
+		$this->setVideoName($this->_videoErrorName);
+		return $this->getLocalFiles();
 	    }
 	}
+
+	return $this->check_founded_files();
+    }
+
+    /**
+     * getFiles 
+     * 
+     * @access protected
+     * @return bool
+     */
+    protected function getFiles() {
+	// Get the files, video and picture to use
+	if($this->_externalVideo){
+	    // Videos are store remotely
+	    return $this->getExternalFiles();
+	} else {
+	    // Videos are store locally
+	    return $this->getLocalFiles();
+	}
+    }
+
+    /**
+     * Check if an url is existed
+     * Take from here:
+     * http://www.php.net/manual/en/function.file-exists.php#76420
+     *
+     * @param  string    $url
+     * @return bool      True if the url is accessible and false if the url is unaccessible or does not exist
+     * @throws Exception An exception will be thrown when Curl session fails to start
+     */
+    protected function url_exists($url)
+    {
+        if (null === $url || '' === trim($url))
+        {
+            throw new Exception('The url to check must be a not empty string');
+        }
+       
+        $handle   = curl_init($url);
+
+        if (false === $handle)
+        {
+            throw new Exception('Fail to start Curl session');
+        }
+
+        curl_setopt($handle, CURLOPT_HEADER, false);
+        curl_setopt($handle, CURLOPT_NOBODY, true);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+
+        // grab Url
+        $connectable = curl_exec($handle);
+
+        // close Curl resource, and free up system resources
+        curl_close($handle);   
+        return $connectable;
     }
 
     /**
@@ -91,11 +237,34 @@ class itheora {
      * 
      * @param string $videoName 
      * @access public
-     * @return void
+     * @return bool             True if videoName is set, false if there are some errors
      */
     public function setVideoName($videoName) {
-	$this->_videoName = $videoName;
-	$this->getFiles();
+	$this->_files = array();
+	if($this->url_exists($videoName)){
+	    // Check if is supported video
+	    if($this->is_supported_video(get_headers($videoName, 1))) {
+		// Ok url is valid, the video is supported and store remotely
+		$this->_externalVideo = true;
+		$url = parse_url($videoName);
+		$this->_videoName = pathinfo($url['path'], PATHINFO_FILENAME);
+		$this->_externalUrl = str_replace(pathinfo($url['path'], PATHINFO_BASENAME), '', $url['scheme'] . '://' . $url['host'] . $url['path']);
+	    } else {
+		$this->_videoName = $this->_videoErrorName;
+	    }
+	    // Now get and search other files
+	    return $this->getFiles();
+	} else {
+	    // It is not a valid url, check if it is a valid name and get file
+	    if(preg_match('/^[a-z0-9._-]+$/i', $videoName)){
+		$this->_videoName = $videoName;
+	    } else {
+		$this->_videoName = $this->_videoErrorName;
+	    }
+	    return $this->getFiles();
+	}
+
+	return false;
     }
 
     /**
@@ -157,7 +326,10 @@ class itheora {
      * @access public
      * @return string | false
      */
-    public function getPoster( $filetypes = array('png', 'jpg', 'gif') ) {
+    public function getPoster( $filetypes = null ) {
+	if($filetypes === null){
+	    $filetypes = $this->_supported_image;
+	}
 	if(is_array($filetypes)){
 	    foreach( $filetypes as $filetype ) {
 		if(isset($this->_files[$filetype]))
@@ -177,11 +349,18 @@ class itheora {
      * @access public
      * @return array
      */
-    public function getPosterSize( $filetypes = array('png', 'jpg', 'gif') ) {
+    public function getPosterSize( $filetypes = null ) {
+	if($filetypes == null){
+	    $filetypes = $this->_supported_image;
+	}
 	if(is_array($filetypes)){
 	    foreach( $filetypes as $filetype ) {
 		if(isset($this->_files[$filetype]))
-		    return getimagesize($this->_videoStoreDir . '/' . $this->_videoName .  '/' .$this->_files[$filetype]);
+		    if($this->_externalVideo){
+			return getimagesize($this->completeUrl($this->_files[$filetype]));
+		    } else {
+			return getimagesize($this->_videoStoreDir . '/' . $this->_videoName .  '/' .$this->_files[$filetype]);
+		    }
 	    }
 	}
 
